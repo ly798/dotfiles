0d343c4be306e48dd1496d7f3d2085a61b860185
LiuYang <yippeetry@gmail.com>
2016-08-08 21:09:40 +0800

    terminator
    Signed-off-by: LiuYang <yippeetry@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,3 +9,5 @@ ln -s $DIR/zshr/oh-my-zsh $HOME/.oh-my-zsh
 echo "git..."
 ln -s $DIR/git/gitconfig $home/.gitconfig
 
+echo "terminator..."
+ln -s $DIR/terminator/config $HOME/.config/terminator/config
