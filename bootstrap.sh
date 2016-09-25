a7f70166f62897cc32198886d311dbf3e6f7b885
LiuYang <yippeetry@gmail.com>
2016-09-25 10:33:15 +0800

    add atom dotfiles
    Signed-off-by: LiuYang <yippeetry@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -22,7 +22,7 @@ ln -s $DIR/zsh/zshrc $HOME/.zshrc
 
 if [ -L $HOME/.oh-my-zsh ];then
     unlink $HOME/.oh-my-zsh
-elif [ -f $HOME/.oh-my-zsh ];then
+elif [ -d $HOME/.oh-my-zsh ];then
     mv $HOME/.oh-my-zsh $BACK_DIR/.oh-my-zsh.$DAY
 fi
 ln -s $DIR/zsh/oh-my-zsh $HOME/.oh-my-zsh
@@ -61,3 +61,35 @@ elif [ -f $HOME/.pip/pip.conf ];then
     mv $HOME/.pip/pip.conf $BACK_DIR/pip.conf.$DAY
 fi
 ln -s $DIR/source/pip.conf $HOME/.pip/pip.conf
+
+echo "atom..."
+if [ -L $HOME/.atom/init.coffee ];then
+    unlink $HOME/.atom/init.coffee
+elif [ -f $HOME/.atom/init.coffee ];then
+    mv $HOME/.atom/init.coffee $BACK_DIR/atom.init.coffee.$DAY
+fi
+if [ -L $HOME/.atom/config.cson ];then
+    unlink $HOME/.atom/config.cson
+elif [ -f $HOME/.atom/config.cson ];then
+    mv $HOME/.atom/config.cson $BACK_DIR/atom.config.cson.$DAY
+fi
+if [ -L $HOME/.atom/keymap.cson ];then
+    unlink $HOME/.atom/keymap.cson
+elif [ -f $HOME/.atom/keymap.cson ];then
+    mv $HOME/.atom/keymap.cson $BACK_DIR/atom.keymap.cson.$DAY
+fi
+if [ -L $HOME/.atom/snippets.cson ];then
+    unlink $HOME/.atom/snippets.cson
+elif [ -f $HOME/.atom/snippets.cson ];then
+    mv $HOME/.atom/snippets.cson $BACK_DIR/atom.snippets.cson.$DAY
+fi
+if [ -L $HOME/.atom/styles.less ];then
+    unlink $HOME/.atom/styles.less
+elif [ -f $HOME/.atom/styles.less ];then
+    mv $HOME/.atom/styles.less $BACK_DIR/atom.styles.less.$DAY
+fi
+ln -s $DIR/atom/init.coffee $HOME/.atom/init.coffee
+ln -s $DIR/atom/config.cson $HOME/.atom/config.cson
+ln -s $DIR/atom/keymap.cson $HOME/.atom/keymap.cson
+ln -s $DIR/atom/snippets.cson $HOME/.atom/snippets.cson
+ln -s $DIR/atom/styles.less $HOME/.atom/styles.less
