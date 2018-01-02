25c8e3303144a31604e6d8c205d32dd210c97ac1
LiuYang <yippeetry@gmail.com>
2018-01-02 16:49:45 +0800

    fix neotree projectile
    Signed-off-by: LiuYang <yippeetry@gmail.com>

--- emacs/.emacs.d/lisp/init-neotree.el
+++ emacs/.emacs.d/lisp/init-neotree.el
@@ -3,17 +3,17 @@
 (defun neotree-project-dir ()
   "Open NeoTree using the git root."
   (interactive)
-  (if projectile-project-root
-      (let ((project-dir (projectile-project-root))
-            (file-name (buffer-file-name)))
-        (neotree-toggle)
+  (let ((project-dir (ignore-errors (projectile-project-root)))
+        (file-name (buffer-file-name))
+        (neo-smart-open t))
+    (if (neo-global--window-exists-p)
+        (neotree-hide)
+      (progn
+        (neotree-show)
         (if project-dir
-            (if (neo-global--window-exists-p)
-                (progn
-                  (neotree-dir project-dir)
-                  (neotree-find file-name)))
-          (message "Could not find git project root.")))
-    (neotree-toggle)))
+            (neotree-dir project-dir))
+        (if file-name
+            (neotree-find file-name))))))
 (global-set-key [f8] 'neotree-project-dir)
 
 (add-hook 'neotree-mode-hook
